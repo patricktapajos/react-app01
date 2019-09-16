5a8bcc722166b29d004275b14252f54f2e0ecc2a
patrick <patrick@lazarus>
2019-09-16 09:50:02 -0400

    Adicao de arquivo gitignore

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React, { Component, Fragment } from 'react'
+import { render } from 'react-dom'
+import Button from './Button'
+import './style.scss'
+
+class App extends Component {
+  state = {
+    counter: 0
+  }
+  handleClick = () => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+  render() {
+    return (
+      <Fragment>
+        <h1 className="title"> Olá</h1>
+        <h2>{this.state.counter}</h2>
+        <Button onClick={this.handleClick}>Enviar</Button>
+      </Fragment>
+    )
+  }
+}
+
+render(<App />, document.getElementById('app'))
